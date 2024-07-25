095f2d666728e49ed7855db28361e5df61b38a54
musatotan <musatotan@gmail.com>
2024-07-25 17:04:15 +0300

    ecosystem.config.js has been added.

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps: [
+        {
+            name: "piri.subchains.installer",
+            script: "npm",
+            args: "start",
+            log_date_format: "DD-MM-YYYY HH:mm"
+        }
+    ]
+};
